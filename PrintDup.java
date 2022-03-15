798f29115413ddc1c8a910a0cf1d5ed51f4cecf3
MuthuSubalakshmi <100410577+MuthuSubalakshmi@users.noreply.github.com>
2022-03-15 13:13:11 +0530

    Add files via upload

---
+++ PrintDup.java
@@ -0,0 +1,27 @@
+package week3.day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PrintDup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> listContainDuplicate= new ArrayList<Integer>(Arrays.asList(14,12,13,11,15,14,18,16,17,19,18,17,20));
+
+		Collections.sort(listContainDuplicate);
+		
+		Set<Integer> set = new HashSet<Integer>();
+		for(Integer duplicate : listContainDuplicate) {
+			if(!set.add(duplicate)) {
+				System.out.println(duplicate);
+			}
+
+		}
+	}
+
+}
